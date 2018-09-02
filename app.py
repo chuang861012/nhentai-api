512137ff4ac8cb06600d98f2636ca6c6e05ece60
chuang] <33791604+chuang861012@users.noreply.github.com>
2018-09-02 23:16:14 +0800

    change endpoints

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, jsonify, request,render_template
 from flask_cors import CORS
-from modules.nhentai import get
+from modules.nhentai import search,getBookById
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/get')
-def nhentai():
+@app.route('/search')
+def searchBooks():
     page = request.args.get('page')
     query = request.args.get('query')
-    return get(query,page)
+    return search(query,page)
+
+@app.route('/book')
+def getBook():
+    _id = request.args.get('id')
+    return getBookById(_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- modules/nhentai.py
+++ modules/nhentai.py
@@ -1,13 +1,19 @@
 import requests,json
 
-def get(query=None,page=1):
+def search(query=None,page=1):
     if query is not None:
         url = f"https://nhentai.net/api/galleries/search?query={query}&page={page}&sort=date"
     else:
         url = f"https://nhentai.net/api/galleries/all?page={page}&sort=date"
-    print(url)
+    res = requests.get(url).content.decode()
+    return res
+
+def getBookById(_id):
+    if _id == None:
+        return "id required."
+    url = f'https://nhentai.net/api/gallery/{_id}'
     res = requests.get(url).content.decode()
     return res
 
 if __name__ == "__main__":
-    print(get())
+    print(getBookById(245031))
